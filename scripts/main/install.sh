e51dc51daae05ddbc093d6bcd29ec1a995091c74
Dominik Schwaiger <51075975+quiode@users.noreply.github.com>
2021-11-02 21:13:05 +0100

    install + yayx install

--- scripts/main/install.sh
+++ scripts/main/install.sh
@@ -1,2 +1,20 @@
 #!/bin/bash
 # This script installs all packages and configfiles, removes old config filesset
+
+# Constants
+SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+SCRIPT_DIR=$SCRIPT_DIR"/../../"
+# Variables
+unalias cp
+
+# YAY
+bash "$SCRIPT_DIR"/scripts/sub/yay_setup.sh
+
+# Install Packages
+bash "$SCRIPT_DIR"/scripts/sub/install_yay_packages.sh
+
+# One Time Setup
+bash "$SCRIPT_DIR"/scripts/sub/one-time-setup.sh
+
+# Install Configs
+bash "$SCRIPT_DIR"/scripts/sub/install_configs.sh
--- scripts/sub/install_programs.sh
+++ scripts/sub/install_programs.sh
@@ -1,2 +1,8 @@
 #!/bin/bash
 # installs programs that can't be installed with yay
+filepath=$SCRIPT_DIR"/packages/yay.txt"
+packages=''
+while read -r line; do
+    packages="$packages $line"
+done <"$filepath"
+yay -S "$packages"
