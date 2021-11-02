8362c154daf4adc8374f6bf662b6def14245e442
Dominik Schwaiger <51075975+quiode@users.noreply.github.com>
2021-11-02 21:25:59 +0100

    grammer

--- scripts/main/install.sh
+++ scripts/main/install.sh
@@ -14,7 +14,7 @@ bash "$SCRIPT_DIR"/scripts/sub/yay_setup.sh
 bash "$SCRIPT_DIR"/scripts/sub/install_yay_packages.sh
 
 # Install programs that can't be installed with yay
-bash "$SCRIPT_DIR"/scripts/sub/install_programms.sh
+bash "$SCRIPT_DIR"/scripts/sub/install_programs.sh
 
 # One Time Setup
 bash "$SCRIPT_DIR"/scripts/sub/one-time-setup.sh
