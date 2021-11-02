2dcd5f1b372ccc33e6d64e652c2f5ffb8947018c
Dominik Schwaiger <51075975+quiode@users.noreply.github.com>
2021-11-02 21:31:04 +0100

    better packages

--- scripts/sub/install_yay_packages.sh
+++ scripts/sub/install_yay_packages.sh
@@ -10,6 +10,6 @@ unalias cp
 filepath=$SCRIPT_DIR"/packages/yay.txt"
 packages=''
 while read -r line; do
-    packages="$packages $line"
+    packages=$packages $line
 done <"$filepath"
 yay -S "$packages"
