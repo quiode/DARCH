b1ce34c62f99f013e02e009828382f3c3708109c
Dominik Schwaiger <51075975+quiode@users.noreply.github.com>
2021-11-02 21:22:44 +0100

    yay packages

--- scripts/sub/install_yay_packages.sh
+++ scripts/sub/install_yay_packages.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 # installs all packages that can be installed with yay
+
+# Constants
+SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+SCRIPT_DIR=$SCRIPT_DIR"/../../"
+# Variables
+unalias cp
+
 filepath=$SCRIPT_DIR"/packages/yay.txt"
 packages=''
 while read -r line; do
