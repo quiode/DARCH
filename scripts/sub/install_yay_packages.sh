638069f18a2c8e7f5c314d6183339787e61cfabb
Dominik Schwaiger <51075975+quiode@users.noreply.github.com>
2021-11-02 21:35:11 +0100

    yay packages list update

--- scripts/sub/install_yay_packages.sh
+++ scripts/sub/install_yay_packages.sh
@@ -8,8 +8,4 @@ SCRIPT_DIR=$SCRIPT_DIR"/../../"
 unalias cp
 
 filepath=$SCRIPT_DIR"/packages/yay.txt"
-packages=''
-while read -r line; do
-    packages=$packages $line
-done <"$filepath"
-yay -S "$packages"
+xargs yay -S <"$filepath"
