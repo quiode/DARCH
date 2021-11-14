7423abef9e1b5de7176742ba0e5330c6e20a5a73
Dominik Schwaiger <domi.schwaiger04@gmail.com>
2021-11-14 16:21:53 +0100

    sddm

--- scripts/sub/one-time-setup.sh
+++ scripts/sub/one-time-setup.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 # one time setup, has to be run once and then never again, but doesn't break stuff if run multiple times
 # use zsh terminal
-sudo -s
-
-chsh -s /bin/zsh root
-chsh -s /bin/zsh "$USER"
+sudo chsh -s /bin/zsh root
+sudo chsh -s /bin/zsh "$USER"
 # download and install oh-my-zsh
 cd "$HOME" || exit
 wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
@@ -32,3 +30,4 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
 
 # systemctl
 sudo systemctl enable ntpd.service
+sudo systemctl start sddm.service
