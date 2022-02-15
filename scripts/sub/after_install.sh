4446fca37fe51adc245d096043633ea35544f423
Dominik Schwaiger <51075975+quiode@users.noreply.github.com>
2022-02-15 21:14:15 +0100

    remove darch folder after install

--- scripts/sub/after_install.sh
+++ scripts/sub/after_install.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+# Removes DARCH folder if it is in the home folder
+cd "$HOME" || exit
+if [[ -d DARCH ]]; then
+    sudo rm -r DARCH
+fi
 # Gives message after install
 echo -e "\e[00;34m- Set dracula theme in tilix.\e[00m"
--- scripts/sub/install_programs.sh
+++ scripts/sub/install_programs.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # installs programs that can't be installed with yay
+# doesn't break if a program is already installed
 
 # Installs my scripts
 cd "$HOME" || exit
