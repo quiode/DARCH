ef37f2c82bb24cec685a27a90c2bb587d6886be7
Dominik Schwaiger <51075975+quiode@users.noreply.github.com>
2022-02-15 20:47:28 +0100

    small fix

--- scripts/sub/install_programs.sh
+++ scripts/sub/install_programs.sh
@@ -2,6 +2,8 @@
 # installs programs that can't be installed with yay
 
 # Installs my scripts
+cd "$HOME" || exit
+
 git clone https://github.com/quiode/scripts
 sudo chmod +x scripts/system-update/update-package-managers.sh
 sudo mv scripts/system-update/update-package-managers.sh /opt
